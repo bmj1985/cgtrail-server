a98b17ecdb460de94f2e522de23114d8f9997957
Brandon Johnson <bmj1985@gmail.com>
2018-04-17 17:26:32 -0600

    added heroku env uri

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
 const app = express()
 
 const morgan = require('morgan')
@@ -13,9 +12,29 @@ app.use(cors())
 
 app.use('/api/alpha/businesses', require('./routes/routes'))
 
+// const uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/HelloMongoose'
+
+// // The http server will listen to an appropriate port, or default to
+// // port 5000.
+// const theport = process.env.PORT || 5000
+
+// // Makes connection asynchronously.  Mongoose will queue up database
+// // operations and release them when the connection is complete.
+// mongoose.connect(uristring, function (err, res) {
+//   if (err) {
+//     console.log('ERROR connecting to: ' + uristring + '. ' + err)
+//   } else {
+//     console.log('Succeeded connected to: ' + uristring)
+//   }
+// })
+require('dotenv').load()
+const MONGODB = process.env.MONGODB_URI
+console.log(MONGODB)
 mongoose.Promise = global.Promise
 if (process.env.NODE_ENV !== 'test') {
-  mongoose.connect('mongodb://localhost/businesses')
+  mongoose.connect(MONGODB)
+  const db = mongoose.connection
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 }
 
 app
