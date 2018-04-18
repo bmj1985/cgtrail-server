32b24724e9d8fca2af361b8b999b0416c1020c55
Brandon Johnson <bmj1985@gmail.com>
2018-04-17 22:24:59 -0600

    messing around with mongodbconnection

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').load()
 const app = express()
 
 const morgan = require('morgan')
@@ -12,30 +13,11 @@ app.use(cors())
 
 app.use('/api/alpha/businesses', require('./routes/routes'))
 
-// const uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/HelloMongoose'
-
-// // The http server will listen to an appropriate port, or default to
-// // port 5000.
-// const theport = process.env.PORT || 5000
-
-// // Makes connection asynchronously.  Mongoose will queue up database
-// // operations and release them when the connection is complete.
-// mongoose.connect(uristring, function (err, res) {
-//   if (err) {
-//     console.log('ERROR connecting to: ' + uristring + '. ' + err)
-//   } else {
-//     console.log('Succeeded connected to: ' + uristring)
-//   }
-// })
-require('dotenv').load()
 const MONGODB = process.env.MONGODB_URI
-console.log(MONGODB)
+mongoose.connect(mongoDB)
 mongoose.Promise = global.Promise
-if (process.env.NODE_ENV !== 'test') {
-  mongoose.connect(MONGODB)
-  const db = mongoose.connection
-  db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-}
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app
   .use((error, request, response, next) => {
